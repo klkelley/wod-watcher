79a0623e094c085ad5c7c03dc39350bd7982478e
klkelley <kelley.15@gmail.com>
2019-01-25 19:39:54 -0600

    Add formatting for WOD description

--- src/main/scala/me/karakelley/Wod.scala
+++ src/main/scala/me/karakelley/Wod.scala
@@ -1,5 +1,7 @@
 package me.karakelley
 
+import scala.util.matching.Regex
+
 case class Wod(
   date: String,
   description: String,
@@ -7,15 +9,33 @@ case class Wod(
 )
 
 object Wod {
-  implicit def converter(wod: Wod) = {
-    val pattern = "<p>(.*?)</p>".r
-    val formattedWodDescription = pattern
-      .findAllIn(wod.description)
+  def apply(date: String, description: String, link: String): Wod = new Wod(date, format(description), link)
+
+  private def format(description: String) = {
+    val noTags = removeHtmlTags(description)
+    val noWhiteSpace = stripWhiteSpaces(noTags)
+    formatWodHeading(stripWhiteSpaces(removeHtmlTags(description)))
+  }
+
+  private def removeHtmlTags(description: String) = {
+    val pTagPattern: Regex = "<p>(.*?)</p>".r
+    val formattedWodDescription = pTagPattern
+      .findAllIn(description)
       .matchData
       .flatMap{m => m.subgroups}
       .toList
-      .head
+      .headOption
 
-    wod.copy(description = formattedWodDescription)
+    formattedWodDescription.getOrElse(description)
+  }
+
+  private def stripWhiteSpaces(description: String) = {
+    val pattern: Regex = "\\s{2}".r
+    pattern.replaceAllIn(description, "\n\n")
+  }
+
+  private def formatWodHeading(description: String) = {
+    val colonPattern: Regex = "\\:".r
+    colonPattern.replaceAllIn(description, "\n")
   }
 }
